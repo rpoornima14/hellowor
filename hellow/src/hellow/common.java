7e9242fc0d61c722843b10dcd018dc8a48ae1d20
rpoornima14 <poornimagder@gmail.com>
2020-10-09 19:35:08 +0530

    hi commiy

---
+++ hellow/src/hellow/common.java
@@ -0,0 +1,10 @@
+package hellow;
+
+public class common {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ hellow/src/hellow/hellow2.java
@@ -0,0 +1,10 @@
+package hellow;
+
+public class hellow2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi2");
+	}
+
+}
